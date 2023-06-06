f3805caef31130d34daaabcefbbd5eb43b7f217c
ngobiea <70290199+ngobiea@users.noreply.github.com>
2023-06-07 01:23:51 +0500

    mongodb store to file store

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
+const FileStore = require("session-file-store")(session);
+
 
 const userRoutes = require("./routes/userRoute");
 const shoppingCartRoutes = require("./routes/shoppingCartRoute");
@@ -10,17 +11,14 @@ const shoppingCartRoutes = require("./routes/shoppingCartRoute");
 const app = express();
 app.use(bodyParser.json());
 
-const store = new MongoDBStore({
-  uri: "mongodb://127.0.0.1:27017/assignment",
-  collection: "sessions",
-});
+
 
 app.use(
   session({
     secret: "my secret",
     resave: false,
     saveUninitialized: false,
-    store,
+    store: new FileStore(),
   }),
 );
 
